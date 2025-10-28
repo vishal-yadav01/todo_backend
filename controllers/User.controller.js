d8054ddda641d55f4dfb2712418b015a34c9e73b
unknown <ydev2897@gmail.com>
2025-10-28 16:14:43 +0530

    Updated backend cookie and CORS configuration for deployment

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -39,7 +39,7 @@ exports.signup = async (req, res) => {
 
       res.cookie('token', token, {
         httpOnly: true,
-        sameSite: 'none',
+        sameSite: 'None',
         secure: true,
         maxAge: 2 * 24 * 60 * 60 * 1000,
       });
